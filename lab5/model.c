c606ceaf07b9105d9b2bba540fd20fe7fd49c74a
Kirill Polushin <kirill@polushin.org>
2018-05-15 19:03:11 +0700

    Added model in lab 5

---
+++ lab5/model.c
@@ -0,0 +1,88 @@
+
+#include <stdlib.h>
+#include <errno.h>
+
+#include "model.h"
+
+bool model_init(model_t* model,
+                size_t tasks_num,
+                generator_data_t data,
+                task_t (*generator)(generator_data_t*)) {
+  if (!model || !generator)
+    return false;
+
+  if (pthread_mutex_init(&model->mutex, NULL))
+    return false;
+
+  if (!(model->tasks = (task_t*) malloc(sizeof(task_t) * tasks_num))) {
+    int error = errno;
+    pthread_mutex_destroy(&model->mutex);
+    errno = error;
+    return false;
+  }
+
+  model->tasks_pos = tasks_num - 1;
+  model->tasks_size = tasks_num;
+
+  for (size_t i = 0; i < tasks_num; i++) {
+    model->tasks[i] = generator(&data);
+    data.iteration++;
+  }
+
+  return true;
+}
+
+void model_release(model_t* model) {
+  if (!model || pthread_mutex_trylock(&model->mutex))
+    return;
+
+  pthread_mutex_destroy(&model->mutex);
+  free(model->tasks);
+}
+
+bool model_steal_task(model_t* model, task_t* task) {
+  if (!model)
+    return false;
+
+  if (pthread_mutex_lock(&model->mutex))
+    return false;
+
+  if (!model->tasks_pos)
+    return false;
+
+  (*task) = model->tasks[model->tasks_pos--];
+
+  pthread_mutex_unlock(&model->mutex);
+  return true;
+}
+
+bool model_add_task(model_t* model, task_t* task) {
+  if (!model)
+    return false;
+
+  if (pthread_mutex_lock(&model->mutex))
+    return false;
+
+  if (model->tasks_pos + 1 >= model->tasks_size)
+    return false;
+
+  model->tasks[model->tasks_pos++] = (*task);
+
+  pthread_mutex_unlock(&model->mutex);
+  return true;
+}
+
+size_t model_tasks_count(model_t* model) {
+  size_t value;
+  if (!model)
+    return 0;
+
+  if (pthread_mutex_lock(&model->mutex))
+    return 0;
+
+  value = model->tasks_pos;
+
+  pthread_mutex_unlock(&model->mutex);
+  return value;
+}
+
---
+++ lab5/model.h
@@ -0,0 +1,78 @@
+
+#ifndef _MODEL_H
+#define _MODEL_H
+
+#include <pthread.h>
+#include <stdbool.h>
+
+typedef struct generator_data {
+  size_t iteration;
+  size_t rank;
+  size_t processes;
+} generator_data_t;
+
+typedef size_t task_t;
+
+typedef struct model {
+  pthread_mutex_t mutex;
+  size_t tasks_pos;
+  size_t tasks_size;
+  task_t* tasks;
+} model_t;
+
+/**
+ * Инициализирует модель и заполняет первоначальный список задач при помощи
+ * генератора задач.
+ *
+ * @param model Модель для инициализации.
+ * @param tasks_num Количество задач, которые нужно сгенерировать.
+ * @param data Начальные данные для генерации задач.
+ * @param generator Генератор задач.
+ *
+ * @return true, если инициализация прошла успешно.
+ */
+bool model_init(model_t* model,
+                size_t tasks_num,
+                generator_data_t data,
+                task_t (*generator)(generator_data_t*));
+
+/**
+ * Освобождает сохраненные данные в модели.
+ *
+ * @param model Модель для освобождения.
+ */
+void model_release(model_t* model);
+
+/**
+ * Забирает одну задачу из списка задач.
+ * Если задач в списке нет, возвращает false.
+ *
+ * @param model Модель.
+ * @param task Результат получения задачи.
+ *
+ * @return true, если задача была забрана.
+ */
+bool model_steal_task(model_t* model, task_t* task);
+
+/**
+ * Добавляет задачу в список задач.
+ * Если места в списке нет, возвращает false.
+ *
+ * @param model Модель.
+ * @param task Добавляемая задача.
+ *
+ * @return true, если задача была добавлена.
+ */
+bool model_add_task(model_t* model, task_t* task);
+
+/**
+ * Получает количество доступных задач.
+ *
+ * @param model Модель.
+ *
+ * @return Количество доступных задач.
+ */
+size_t model_tasks_count(model_t* model);
+
+#endif
+
